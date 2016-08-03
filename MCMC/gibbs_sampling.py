1d110b832aa753327e6cf7981b106fc906663a7e
davidk <davidk@DESKTOP-A16PUTH>
2016-08-03 17:55:49 +0900

    gibbs_sampling

--- MCMC/gibbs_sampling.py
+++ MCMC/gibbs_sampling.py
@@ -1,20 +1,24 @@
 # refer https://theclevermachine.wordpress.com/2012/11/05/mcmc-the-gibbs-sampler/
 import numpy as np
-from scipy.ndimage.measurements import variance
+# from scipy.ndimage.measurements import variance
 
 # Example : GIBBS sampler for bivariate_normal
 nSamples = 5000
 
 mu = np.array([0 , 0])
-rho = np.array([[0.8],[0.8]])
-
+rho = np.array([[0.8],[0.8]]) # ([rho_21, rho_12])
 # Initialize the Gibbs sampler
 propSigma = 1 # Proposal variance
 minn = np.array([-3 , -3])
 maxx = np.array([3  ,  3])
 
+# Initialize samples
+x= np.zeros((nSamples,2))
+x[0,0]= 3*np.random.randn() -3
+x[0,1]= 3*np.random.randn() -3
+
 # Run GIBBS Sampler
-t= 1
+t= 0
 
 while t < nSamples:
     t= t + 1
@@ -23,4 +27,5 @@ while t < nSamples:
         # Update Samples  
         nIx = iD
         # Conditional mean
-        muCond = mu(iD) + rho(iD)*(x(T))
+        muCond = mu(iD) + rho(iD)*(x(T(id), nIx) - mu(nIx))
+        
