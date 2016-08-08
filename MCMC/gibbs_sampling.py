3ba6f3154f81c417545c6857e3e6fc50c4ecb144
davidk <davidk@DESKTOP-A16PUTH>
2016-08-08 11:32:32 +0900

    gibbs_sampling

--- MCMC/gibbs_sampling.py
+++ MCMC/gibbs_sampling.py
@@ -10,12 +10,16 @@ rho = np.array([[0.8],[0.8]]) # ([rho_21, rho_12])
 # Initialize the Gibbs sampler
 propSigma = 1 # Proposal variance
 minn = np.array([-3 , -3])
-maxx = np.array([3  ,  3])
+maxx = np.array([ 3 ,  3])
 
 # Initialize samples
 x= np.zeros((nSamples,2))
 x[0,0]= 3*np.random.randn() -3
 x[0,1]= 3*np.random.randn() -3
+
+
+dims = np.array([1, 2])
+
 
 # Run GIBBS Sampler
 t= 0
@@ -25,7 +29,11 @@ while t < nSamples:
     T= np.array([[t-1, t]])
     for iD in range(1,3) : # loop over dimensions
         # Update Samples  
-        nIx = iD
+        nIx = dims != iD 
         # Conditional mean
-        muCond = mu(iD) + rho(iD)*(x(T(id), nIx) - mu(nIx))
+        muCond = mu(iD-1) + rho(iD-1)*(x(T(id-1), nIx) - mu(nIx))
+        # Conditional variance
+        varCond = np.math.sqrt(1-np.math.pow(rho(iD)))
+        # Draw from conditional 
+        # 
         
