5c17d00f571e0b698d42a7d646718e43b6f60248
root <root@davidk-WA50SJQ>
2016-08-17 08:06:43 +0900

    ...

--- Generalization_of_multiple_layers_ch5/ch5_main.py
+++ Generalization_of_multiple_layers_ch5/ch5_main.py
@@ -14,4 +14,28 @@ import itertools
 import collections
 
 
+# load the data from scikit-learn
+digits = datasets.load_digits()
 
+# Load the targets
+# Note that the targets are stored as digits, thest need to be
+# converted to one-hot-encoding for the output softmax layer
+T = np.zeros((digits.target.shape[0], 10))
+print "len_of_T :", len(T)
+T[np.arange(len(T)), digits.target] +=1
+
+##############################################################################
+ex=np.zeros((5,5))
+col=np.array([0,1,4,3,2])
+print col
+ex[np.arange(len(ex)), col]+=1
+print ex
+
+
+##############################################################################
+# Divie the data into a train and test set
+X_train, X_test, T_train, T_test = cross_validation.train_test_split(digits.data, T, test_size=0.4)
+
+print "X_train\n"
+print X_train
+                                                                    
